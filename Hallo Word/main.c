1d6c563eb7d8b7a4964db0f473b2f37670e2968f
20161104585 <1663512557@qq.com>
2017-06-07 10:31:56 +0800

    Initial Commit

---
+++ Hallo Word/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Hallo Word
+//
+//  Created by 20161104585 on 17/6/7.
+//  Copyright © 2017年 20161104585. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
